26d3ca3189d92150dc5462bcae9f06a653cd96fb
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-01-08 16:52:53 -0500

    modified app.js turning class into a function

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App () {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: ''
+  //   }
+  // }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
-  }
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(response=> response.json())
+  //     .then(users => {this.setState({ robots: users})});
+  // }
 
-  onSearchChange = (event) => {
+  const onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
   }
 
-  render() {
     const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
@@ -33,13 +32,13 @@ class App extends Component {
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
         </div>
       );
   }
-}
+
 
 export default App;
